5f1e3134cc07f5f3af65b64b1acf7ad0bb015d0f
Flavia Ursino <flavia.ursino@gmail.com>
2022-07-06 12:00:14 -0300

    funcionamiento del login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 router.get('/', function (req, res, next) {
     res.render('admin/login',{
         layout:'admin/layout' // admin/layout.hbs
     }); // view/admin/login.hbs
-})
+}); // cierro get
+
+router.post('/', async function(req,res,next){
+
+    try{
+        console.log(req.body);
+        //capturar los datos 
+        var usuario = req.body.usuario; //flavia
+        var password = req.body.password; // 1234
+
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+        if(data != undefined){
+            res.redirect('/admin/novedades')
+        } else{
+            res.render('admin/login',{ // login.hbs
+                layout:'admin/layout',
+                error:true
+            })
+        }
+    }catch(error){
+        console.log(error)
+    }
+
+}); // cierro post
+
 
 
 module.exports = router;
