0aeb29b06e79e2e8b44a1539ff059e0c3285a40f
Flavia Ursino <flavia.ursino@gmail.com>
2022-07-08 11:18:51 -0300

    variables de session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();// para que cargue los datos del archivo .env
+var session = require('express-session');
 
 var indexRouter = require('./routes/index'); // routes/index.js
 var nosotrosRouter = require('./routes/nosotros'); // routes/nosotros.js
@@ -13,10 +14,9 @@ var galeriaRouter = require('./routes/galeria'); // routes/galeria.js
 var novedadesRouter = require('./routes/novedades'); // routes/novedades.js
 var contactoRouter = require('./routes/contacto'); // routes/contacto.js
 var loginRouter = require('./routes/admin/login'); // routes/admin/login.js
-
+var adminRouter = require('./routes/admin/novedades'); // routes/admin/novedades.js
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -27,21 +27,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'sdhjdksjdsjdahdhsjh12458',
+  cookie:{MaxAge:null},
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try {
+    //console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 app.use('/', indexRouter);
-app.use('/nosotros', nosotrosRouter );
+app.use('/nosotros', nosotrosRouter);
 app.use('/servicios', serviciosRouter);
 app.use('/galeria', galeriaRouter);
 app.use('/novedades', novedadesRouter);
 app.use('/contacto', contactoRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
+
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -16,9 +16,15 @@ router.post('/', async function(req,res,next){
         var usuario = req.body.usuario; //flavia
         var password = req.body.password; // 1234
 
+
         var data = await usuariosModel.getUserAndPassword(usuario,password);
+        // var data = select * from usuarios where usuario = 'flavia' and password = md5(1234)
+        //select trae * = columna id, usuario,password
 
         if(data != undefined){
+            req.session.id_usuario = data.id; // 1
+            req.session.nombre = data.usuario; // Flavia
+
             res.redirect('/admin/novedades')
         } else{
             res.render('admin/login',{ // login.hbs
