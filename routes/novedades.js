4c2118f6da641c02ca8e98bebb18f225fbc4cc7c
Flavia Ursino <flavia.ursino@gmail.com>
2022-07-15 12:01:25 -0300

    novedades desde front dinamicas

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModel');
 
-router.get('/', function(req,res,next){
+router.get('/', async function(req,res,next){
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('novedades',{
-        isNovedades:true
-    }); // view/nosotros.hbs
+        isNovedades:true,
+        novedades // tengo los registros para poder imprimirlos en el hbs
+    }); 
 })
 
 
